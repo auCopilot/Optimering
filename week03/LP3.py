ed2e40821c8879455cbcc0f87d129b08a8487241
ThomasLaptop <202108521@post.au.dk>
2026-02-17 14:39:53 +0100

    week3

---
+++ week03/LP3.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pulp as PLP
+
+# Maximization problem
+model = PLP.LpProblem(name="FoodManufacture1", sense=PLP.LpMaximize)
+
+# Weight and volume constraint values
+max_section_weight = [300, 450, 250]
+max_section_volume = [2200, 2800, 1600]
+max_supply = [1000, 800, 1250, 650] # ton
+profits = [1400, 1800, 1600, 1250] # profit / ton
+volumes_ton = [5, 7, 6, 4]
+fraction_weight = [0.3, 0.45, 0.25]
+
+
+# Indcies of variables
+commodity_range = list(range(4))
+section_range = list(range(3))
+
+# Define section and commodity variables
+commodity = PLP.LpVariable.dicts("commodity", commodity_range, lowBound= 0)
+section_commodity = PLP.LpVariable.dicts("section_commodity", (section_range, commodity_range), lowBound=0)
+section_volume = PLP.LpVariable.dicts("section_volume", section_range, lowBound=0)
+total_weight = PLP.LpVariable("total_weight", lowBound=0)
+
+
+
+# Define upper bounds on supply
+for c in commodity:
+    commodity[c].bounds(0, max_supply[c])
+# Define upper bounds on volume
+for s in section_range:
+    section_volume[s].bounds(0, max_section_volume[s])
+
+# Obejective is to maximize profits
+model += PLP.lpSum(profits[i]*commodity[i] for i in commodity_range), "Objective"
+
+# Add the total to output
+model += total_weight == PLP.lpSum(commodity[i] for i in commodity_range)
+
+# Define weight distribution constraint
+for s in section_range:
+    # Fraction constraint
+    model += (PLP.lpSum(section_commodity[s][i] for i in commodity_range) == total_weight*fraction_weight[s],
+              f"FractionConstraint{s}")
+    # Defines the sectional volumes.
+    model += (section_volume[s] == PLP.lpSum(volumes_ton[i] * section_commodity[s][i] for i in commodity_range),
+    f"VolumeConstraint{s}")
+    model += PLP.lpSum(section_commodity[s][i] for i in commodity_range) <= max_section_weight[s], f"WeightConstraint{s}"
+
+
+# Continuity, make sure that the sum of commodities in each section equals to total amount.
+for i in commodity_range:
+    model += commodity[i] == PLP.lpSum(section_commodity[s][i] for s in section_range)
+
+
+
+from CustomFunctions import print_solution
+print_solution(model)
+
+
+
